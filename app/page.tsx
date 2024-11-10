c69987d0f0e78a58c9b636f0a04bd73bd638739c
Saransh Bangar <saranshbangad@gmail.com>
2024-11-10 16:54:50 +0530

    Repo set, auth layout done

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="h1">SkyBox - the best cloud storage services</h1>
+    </div>
+  );
+}
